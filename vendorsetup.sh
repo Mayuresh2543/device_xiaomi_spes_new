c090e53a160dc62807e367d11e16b583f11cb477
mayuresh <mayureshnanal846@gmail.com>
2025-04-23 11:09:59 +0530

    spes: vendorsetup: remove git clone properties

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,16 +12,4 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 # Remove hardware/lineage/compat to avoid conflicts
 rm -rf hardware/lineage/compat/Android.bp
 
-# Clone kernel/vendortree/xiaomi-hardware
-echo -e "${color}Setup kernel,vendor,xiaomi-hardware tree ${end}"
-git clone https://github.com/muralivijay/kernel_xiaomi_sm6225.git -b main kernel/xiaomi/sm6225
-git clone https://github.com/muralivijay/android_vendor_xiaomi_spes.git -b 13.0 vendor/xiaomi/spes
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
 
-# Clone Miuicamera
-echo -e "${color}Setup Miuicamera ${end}"
-git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git -b uvite-sm6225 vendor/xiaomi/camera/
-
-# Clone BCR
-echo -e "${color}Setup BCR ${end}"
-git clone https://github.com/Chaitanyakm/vendor_bcr.git vendor/bcr
